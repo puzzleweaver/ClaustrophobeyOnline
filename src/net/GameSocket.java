f4d3bbda9e3ac81a8198fa09d0bcc7b22d3f39ae
Riley <rileyborgard@gmail.com>
2015-11-29 17:47:14 -0500

    removed unnecessary contruscot

--- src/net/GameSocket.java
+++ src/net/GameSocket.java
@@ -12,16 +12,6 @@ public abstract class GameSocket extends Thread {
 	
 	public static final int PORT = 7890;
 	
-	public GameSocket() {
-		try {
-//			serverIP = InetAddress.getByName("71.46.93.12"); //riley public
-//			serverIP = InetAddress.getByName("50.90.116.229"); //austin public
-//			serverIP = InetAddress.getByName("192.168.1.115"); // riley
-//			serverIP = InetAddress.getByName("192.168.1.109"); //austin
-			serverIP = InetAddress.getByName("192.168.1.109");
-		} catch (Exception e) {}
-	}	
-	
 	public void sendData(byte[] data, InetAddress ipAddress, int port) {
 		DatagramPacket packet = new DatagramPacket(data, data.length, ipAddress, port);
 		try {
